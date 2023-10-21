89efdfba75436200d67446be293c9b5b2544c1e5
Naveen Kumar <nk24528.nk81@gmail.com>
2023-10-21 11:05:05 +0530

    pushing the Day 1 Introduction to Javascript Problems

---
+++ Introduction-to-JS-Variables-and-Data-Types/1.js
@@ -0,0 +1,4 @@
+// Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+console.log("Masai School" + "\n" + "A Transformation in Education" )
---
+++ Introduction-to-JS-Variables-and-Data-Types/4.js
@@ -0,0 +1,17 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+console.log(`                        
+████████████████████████████████████████████████████████████████████████████████████████████
+█▄─▄▄▀█▄─▄▄─█─▄▄▄▄███▄─▄▄─█▄─██─▄█▄─▄─▀█▄─▄███▄─▄█─▄▄▄─█████─▄▄▄▄█─▄▄▄─█─█─█─▄▄─█─▄▄─█▄─▄███
+██─██─██─▄▄▄█▄▄▄▄─████─▄▄▄██─██─███─▄─▀██─██▀██─██─███▀█████▄▄▄▄─█─███▀█─▄─█─██─█─██─██─██▀█
+▀▄▄▄▄▀▀▄▄▄▀▀▀▄▄▄▄▄▀▀▀▄▄▄▀▀▀▀▄▄▄▄▀▀▄▄▄▄▀▀▄▄▄▄▄▀▄▄▄▀▄▄▄▄▄▀▀▀▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▀▄▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▄▀`);
+
+console.log("\n");
+
+console.log("Name" + "    " + "Grade" + "    " + "Section" + "    " + "Roll No." + "    " + 'Maths' + "  " + "English" + "  " + "Science")
+
+console.log("\n");
+
+console.log("Naveen" + "    " + "A" + "         " + "B" + "          " +  1  + "         " + 100 + "     " + 100 + "      " + 100)
